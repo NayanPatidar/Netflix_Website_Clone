93c15dc8e7cff070793d5f6614f771379b55418d
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-16 22:26:31 +0530

    Added FIrebase config files

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+require('dotenv').config();
+
+console.log(process.env.API_KEY);
+
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: "netflix-clone-47904.firebaseapp.com",
+//     projectId: "netflix-clone-47904",
+//     storageBucket: "netflix-clone-47904.appspot.com",
+//     messagingSenderId: "463765950759",
+//     appId: "1:463765950759:web:abfc296e2dfb6446ebd902",
+//     measurementId: "G-RS91PXQ42J"
+//   };
