1327cdf07013ab0aaf4d1b3960113482a723d75e
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-16 07:19:02 +0530

    Added the drop down button

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'src')));
+
 app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/MainPage', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'src', 'MainPage','index.html'));
+});
+
+app.get('/SignIn', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/SignInPage', 'SignIn.html'));
+});
 
 app.listen(port, () => {
     console.log(`Listening of the port ${port}`);
--- src/MainPage/script.js
+++ src/MainPage/script.js
@@ -7,14 +7,35 @@ radioButtons.forEach(function (radioButton) {
 
         if (clickCount === 2) {
             this.checked = !this.checked;
-            clickCount = 0; // Reset the click count after the second click
+            clickCount = 0; 
         }            
     });
 });
 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
 var signInButton = document.getElementById('SignIn');
 signInButton.addEventListener('click', () => {
+    console.log("Clicked on the Sign In");
     if(signInButton.click){
         window.location.href = '/SignIn';
     }
 })
+
