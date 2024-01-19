72a9c4a2b72a7ef1797a0c78785a651c69d3bf1c
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-19 23:43:13 +0530

    Added PlansInfo Route

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const mainRoute = require('./src/routes/main');
 const main_hiRoute = require('./src/routes/main-hi');
 const signinRoute = require('./src/routes/sign');
 const signin_hiRoute = require('./src/routes/sign-hi');
+const plansInfo = require('./src/routes/plansInfo');
+// const signin_hiRoute = require('./src/routes/sign-hi');
 
 app.use('/', mainRoute);
 app.use('/', main_hiRoute);
 app.use('/', signinRoute);
 app.use('/', signin_hiRoute);
+app.use('/', plansInfo);
 
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
