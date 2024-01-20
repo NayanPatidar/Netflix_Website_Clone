b7ac3a426366884e320166a78a1ea9674d3f3b33
Nayan Patidar <nayanpatidar28@gmail.com>
2024-01-20 19:33:47 +0530

    SignUpInfo and SignUp

--- src/routes/signupInfo.js
+++ src/routes/signupInfo.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-router.get('/signup/registration', (req,res) => {
-    const filePath = path.join(__dirname, '..', 'views', 'en', 'signup.html');
+router.get('/signup/form', (req,res) => {
+    const filePath = path.join(__dirname, '..', 'views', 'en', 'signupInfo.html');
     res.sendFile(filePath);
 })
 
